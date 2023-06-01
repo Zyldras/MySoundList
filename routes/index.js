c80ab5fb3620b0238c76fe2b7116551164d13bf2
Zylau <zylau@live.fr>
2023-06-01 18:54:04 +0200

    BDD

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require("../db.js");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -7,17 +8,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.get("/api/users", (req, res, next) => {
-  var sql = "select * from user"
-  var params = []
+  var sql = "select * from user";
+  var params = [];
   db.all(sql, params, (err, rows) => {
       if (err) {
         res.status(400).json({"error":err.message});
+        res.render('shit.html');
         return;
       }
       res.json({
           "message":"success",
           "data":rows
-      })
+      });
     });
 });
 
